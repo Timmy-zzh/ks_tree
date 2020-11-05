c516e49af096d4b23dd5c5bf740096938ad30ad5
Timmy <timmy_zzh@163.com>
2020-11-06 07:41:03 +0800

    jvm 虚拟机栈

---
+++ javalib/src/main/java/com/timmy/javalib/JvmDemo.java
@@ -0,0 +1,21 @@
+package com.timmy.javalib;
+
+public class JvmDemo {
+
+    public static void main(String[] args) {
+        add(0);
+    }
+
+    public static int add(int k) {
+        int a = 1;
+        int b = 2;
+        return k + a + b;
+    }
+
+    public int add1() {
+        int i = 1;
+        int j = 2;
+        int result = i + j;
+        return result + 10;
+    }
+}

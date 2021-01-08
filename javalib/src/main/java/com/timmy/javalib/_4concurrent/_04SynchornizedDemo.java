f5d6b6b3fa0e9c9767ff815585d126b5c7b717c9
Timmy <timmy_zzh@163.com>
2021-01-08 23:00:01 +0800

    ThreadPoolExecute线程池

--- javalib/src/main/java/com/timmy/javalib/_4concurrent/_04SynchornizedDemo.java
+++ javalib/src/main/java/com/timmy/javalib/_4concurrent/_04SynchornizedDemo.java
@@ -1,4 +1,4 @@
-package com.timmy.testlib;
+package com.timmy.javalib._4concurrent;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
---
+++ javalib/src/main/java/com/timmy/javalib/_4concurrent/_05ThreadPool.java
@@ -0,0 +1,94 @@
+package com.timmy.javalib._4concurrent;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class _05ThreadPool {
+
+    public static void main(String[] args) {
+//        singleThreadPool();
+//        cacheThreadPool();
+//        newFixedThreadPool();
+        newScheduledThreadPool();
+    }
+
+    private static void newScheduledThreadPool() {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                Date now = new Date();
+                System.out.println("Thread:" + Thread.currentThread().getName() +
+                        " ,timer:" + now.toString());
+            }
+        }, 500, 500, TimeUnit.MILLISECONDS);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        scheduledExecutorService.shutdown();
+    }
+
+    private static void newFixedThreadPool() {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            final int taskId = i;
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Thread:" + Thread.currentThread().getName() +
+                            " ,running task:" + taskId);
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    private static void cacheThreadPool() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            final int taskId = i;
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Thread:" + Thread.currentThread().getName() +
+                            " ,running task:" + taskId);
+                    try {
+                        // simple io task
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    private static void singleThreadPool() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++) {
+            final int taskId = i;
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Thread:" + Thread.currentThread().getName() +
+                            " ,running task:" + taskId);
+                }
+            });
+        }
+    }
+}

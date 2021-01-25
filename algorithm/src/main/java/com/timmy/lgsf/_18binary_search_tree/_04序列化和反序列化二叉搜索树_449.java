108ca4155e951829db5a209573edf754fd604408
timmy1 <timmy_zzh@163.com>
2021-01-25 11:26:00 +0800

    二叉搜索树

---
+++ algorithm/src/main/java/com/timmy/lgsf/_18binary_search_tree/_04序列化和反序列化二叉搜索树_449.java
@@ -0,0 +1,38 @@
+package com.timmy.lgsf._18binary_search_tree;
+
+import com.timmy.common.TreeNode;
+
+public class _04序列化和反序列化二叉搜索树_449 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private String seralize(TreeNode root) {
+        return "";
+    }
+
+    private TreeNode deseralize(String data) {
+        return null;
+    }
+
+    /**
+     * 序列化是将数据结构或对象转换为一系列位的过程，以便它可以存储在文件或内存缓冲区中，
+     * 或通过网络连接链路传输，以便稍后在同一个或另一个计算机环境中重建。
+     *
+     * 设计一个算法来序列化和反序列化 二叉搜索树 。 对序列化/反序列化算法的工作方式没有限制。
+     * 您只需确保二叉搜索树可以序列化为字符串，并且可以将该字符串反序列化为最初的二叉搜索树。
+     *
+     * 编码的字符串应尽可能紧凑。
+     *
+     * 示例 1：
+     * 输入：root = [2,1,3]
+     * 输出：[2,1,3]
+     *
+     * 示例 2：
+     * 输入：root = []
+     * 输出：[]
+     *
+     * 链接：https://leetcode-cn.com/problems/serialize-and-deserialize-bst
+     */
+}
